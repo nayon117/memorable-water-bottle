7a339bd8283691abfff6fed001ae64075cd687f8
nayon117 <hasibulhasannayon260@gmail.com>
2023-09-11 18:16:39 +0600

    add navber

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,31 @@ export default {
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
-  theme: {
-    extend: {},
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+        
+"primary": "#59f78b",
+        
+"secondary": "#06d8b2",
+        
+"accent": "#d868b8",
+        
+"neutral": "#1b1c22",
+        
+"base-100": "#304155",
+        
+"info": "#3386eb",
+        
+"success": "#28d2c1",
+        
+"warning": "#9a540e",
+        
+"error": "#e72354",
+        },
+      },
+    ],
   },
   plugins: [require("daisyui")],
 }
